c4a835df7cdf40f7c414da2146d848ba4176e7a8
polina-novinski <94848446+polina-novinski@users.noreply.github.com>
2022-04-25 16:48:20 +0300

    Update map.py

--- map.py
+++ map.py
@@ -1,11 +1,5 @@
-import sys
-from io import BytesIO
 import requests
-from PIL import Image
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
 def get_map(geo):
     toponym_to_find = geo
 
@@ -23,12 +17,9 @@ def get_map(geo):
 
 
     json_response = response.json()
-    # Получаем первый топоним из ответа геокодера.
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
-    # Координаты центра топонима:
     toponym_coodrinates = toponym["Point"]["pos"]
-    # Долгота и широта:
     toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
     delta = "0.005"
 
@@ -42,7 +33,3 @@ def get_map(geo):
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
     return response.content
-
-
-
-#get_map("Москва, ул. Ак. Королева, 12")
